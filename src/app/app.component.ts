e4e8d7cea444799369c6e548ee8f2b3a3ba82a94
huyduong <huy.duong@bbv.vn>
2018-10-26 16:55:48 +0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { SwUpdate } from '@angular/service-worker';
 import { DataService } from './data.service';
 
@@ -12,6 +12,13 @@ export class AppComponent {
 
   update: boolean = false;
   joke: any;
+  @ViewChild('myCanvas') myCanvas: ElementRef;
+  public context: CanvasRenderingContext2D;
+
+  ngAfterViewInit(): void {
+    this.context = (<HTMLCanvasElement>this.myCanvas.nativeElement).getContext('2d');
+  }
+
 
   constructor(updates: SwUpdate, private data: DataService) {
     console.log("ehllo 00000");
@@ -27,6 +34,25 @@ export class AppComponent {
     this.data.gimmeJokes().subscribe(res => {
       this.joke = res;
     })
+  }
+
+  onCapture(e) {
+    let reader = new FileReader();
+    let f = e.target.files[0];
+    let contextData = this.context;
+    reader.onload = (function (theFile) {
+      return function (e) {
+        var img = new Image();
+        img.onload = () => {
+          contextData.drawImage(img, 0, 0);
+        }
+        img.src = e.target.result;
+
+      };
+    })(f);
+    reader.readAsDataURL(f);
+
+
   }
 
 }
